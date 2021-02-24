c31fd684b24d8a29a8dec5d5d675386857c7601e
Thierry Nicola <thierry.nicola@doctena.com>
2021-02-24 18:01:23 +0100

    adding tmux oconfiguration basics and symlink

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -5,3 +5,4 @@ mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/neovim/init.vim    ~/.config/nvim/init.vim 
 ln -sf ~/dotfiles/zsh/zprofile /etc/zsh/zprofile
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/tmux/tmux ~/.tmux.conf
